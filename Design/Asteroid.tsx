922154f028d3f322dc31c8722de955e39bcf4a9b
geoffreytacquet <geoffrey.tacquet@gmail.com>
2023-09-07 16:53:52 +0200

    Added design files

--- Design/Asteroid.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Asteroid " tilewidth="16" tileheight="16" tilecount="15000" columns="150">
- <image source="../../../../../../../Downloads/hjm-random_asteroids-v3/hjm-random_asteroids-v3-01.png" width="2400" height="1600"/>
-</tileset>
--- Design/Background.tsx
+++ Design/Background.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Background" tilewidth="16" tileheight="16" tilecount="792" columns="36">
- <image source="../../../../../../../../Downloads/360_F_259161577_CESOycx2KteeRGI7VUUBLDjz76x6MILO.jpg" width="576" height="360"/>
+ <image source="360_F_259161577_CESOycx2KteeRGI7VUUBLDjz76x6MILO.jpg" width="576" height="360"/>
 </tileset>
